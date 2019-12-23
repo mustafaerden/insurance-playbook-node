71357ea7ae6874d0d96d6830515cff317a4b7222
Mustafa Erden <mustafaerden87@gmail.com>
2019-12-24 01:53:05 +0800

    database details removed

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = {mongoDbUrl: 'mongodb://mustafaerden:Me1054620084@ds131983.mlab.com:31983/insurance-playbook'}
+  module.exports = {mongoDbUrl: 'databaseVeSifresiBurayaGelecek'}
 } else {
   module.exports = {mongoDbUrl: 'mongodb://localhost:27017/insurance-playbook'}
 }
